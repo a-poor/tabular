9cdfa6226f153c10efa68c0e95340c42fcf57a1e
a-poor <a-poor@users.noreply.github.com>
2021-01-10 11:41:55 -0500

    Updating tabular.py

--- tabular.py
+++ tabular.py
@@ -1,2 +1,27 @@
+
 import struct
+
+
+# Private Functions
+
+def _getType(t):
+    check = lambda a,b: a==b or isinstance(a,b)
+    if check(t,int): return "l"
+    if check(t,float): return "d"
+    if check(t,bool): return "?"
+    if check(t,str): return "s"
+
+def _maxStrLen(itr):
+    return max(map(len,itr),default=0)
+
+
+# Public Functions
+
+def pack_dicts(lod,fileobj):
+    if len(lod) > 0: return
+    cols = list(lod[0].keys())
+
+
+def unpack(f):
+    pass
 
